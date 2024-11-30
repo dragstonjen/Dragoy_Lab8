7c57a6e369c3b91a6a782ef27b8cd5179f36087e
dragstonjen <dragstonjen@gmail.com>
2024-11-30 11:11:05 +0200

    Update lab8.cpp

--- lab8.cpp
+++ lab8.cpp
@@ -1,38 +1,32 @@
 #include <iostream>
-#include <windows.h>
-#include <string>
+#include <Windows.h>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 void removeEvensAndSortDescending(vector<int>& numbers) {
-    numbers.erase(
-        remove_if(numbers.begin(), numbers.end(), [](int num) { return num % 2 == 0; }),
-        numbers.end()
-    );
-
+    numbers.erase(remove_if(numbers.begin(), numbers.end(), [](int num) { return num % 2 == 0; }), numbers.end());
     sort(numbers.begin(), numbers.end(), greater<int>());
 }
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    vector<int> numbers = { 10, 3, 7, 8, 2, 5, 6, 9 };
+    vector<int> numbers = { 10, 3, 7, 8, 2, 5, 6, 9, 1,-3 };
 
     cout << "Початковий вектор: ";
-    for (int x = 0; x < numbers.size(); x++) {
-        cout << numbers[x] << " ";
+    for (int num : numbers) {
+        cout << num << " ";
     }
     cout << endl;
 
     removeEvensAndSortDescending(numbers);
 
     cout << "Модифікований вектор: ";
-    for (int x = 0; x < numbers.size(); x++) {
-        cout << numbers[x] << " ";
+    for (int num : numbers) {
+        cout << num << " ";
     }
     cout << endl;
-
     return 0;
 }
